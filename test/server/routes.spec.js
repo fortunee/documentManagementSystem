6c2511af8950b78895df40c8ede53c4a55535b0c
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-01 12:10:22 +0100

    Ensure that the users route with params returns a response

--- test/server/routes.spec.js
+++ test/server/routes.spec.js
@@ -27,3 +27,28 @@ describe('Users Route /api/users', () => {
       .expect(200, done);
   });
 });
+
+/**
+ * Test suite to ensure that /users/:username route with a param of
+ * username returns a 200 ok response on a put, get or delete request.
+ */
+describe('users/:username route with a username param', () => {
+  it('Should return a 200 ok response on a put request', (done) => {
+    request
+      .put('/api/users/fortune')
+      .expect(200, done);
+  });
+
+  it('Should return a 200 ok response on a get request', (done) => {
+    request
+      .get('/api/users/fortune')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+  });
+
+  it('Should return a 200 ok response on a delete request', (done) => {
+    request
+      .delete('/api/users/fortune')
+      .expect(200, done);
+  });
+});
