2de5b7dd197c4b37649f3f741a582210a7dc523e
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-09 10:40:54 +0100

    Add a document model

---
+++ app/migrations/20161209092138-create-document.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Documents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      ownerId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Documents');
+  }
+};
---
+++ app/models/document.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Document = sequelize.define('Document', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    ownerId: DataTypes.INTEGER,
+    access: {
+      defaultValue: 'public',
+      type: DataTypes.STRING,
+      validate: {
+        isIn: [['private', 'public', 'role']]
+      }
+    },
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // associations defined here
+        Document.belongsTo(models.User, {
+          as: 'Owner',
+          onDelete: 'CASCADE',
+          foreignKey: { allowNull: false }
+        });
+
+        Document.belongsTo(models.Type, {
+          onDelete: 'CASCADE',
+          foreignKey: { allowNull: true }
+        });
+      }
+    }
+  });
+  return Document;
+};
