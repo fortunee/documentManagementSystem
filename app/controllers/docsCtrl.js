32a2036bc80edf6d8750bf4a63b832c5a9c50d59
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-13 00:24:38 +0100

    Create a new document controller

--- app/controllers/docsCtrl.js
+++ app/controllers/docsCtrl.js
@@ -18,4 +18,20 @@ const docsCtrl = {
       res.send(documents);
     });
   },
+
+  /**
+   * Create a new document
+   * @param {Object} req Request object
+   * @param {Object} res Response object
+   * @returns {Void} Returns Void
+   */
+  createDoc(req, res) {
+    db.Document.create(req.body)
+      .then((document) => {
+        res.status(201).send(document);
+      })
+      .catch((err) => {
+        res.status(400).send(err.errors);
+      });
+  },
 };
