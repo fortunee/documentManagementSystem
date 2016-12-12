e6a8522a2f41207afebad855de3d25f38ec7fc9b
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-12 22:29:36 +0100

    Edit and update a specific role

--- app/controllers/rolesCtrl.js
+++ app/controllers/rolesCtrl.js
@@ -47,6 +47,28 @@ const rolesCtrl = {
       });
   },
 
+  /**
+   * Edit and update a specific role
+   * @param {Object} req Request object
+   * @param {Object} res Response object
+   * @returns {Object} Response Object
+   */
+  editRole(req, res) {
+    db.Role.findById(req.params.id)
+      .then((role) => {
+        if (!role) {
+          return res.status(404)
+            .send({ message: 'Cannot edit a role that does not exist' });
+        }
+
+        role.update(req.body)
+          .then((updatedRole) => {
+            res.send(updatedRole);
+          });
+      });
+  },
+
+
 };
 
 module.exports = rolesCtrl;
