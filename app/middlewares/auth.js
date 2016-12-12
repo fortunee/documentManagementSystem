e246a4b7f2b62f934e7515ec366e6a817175cb52
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-12 20:49:10 +0100

    Add method to verify that the supplied token is valid

---
+++ app/middlewares/auth.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import db from '../models';
+
+
+/**
+ * Secret token for jsonwebtoken
+ */
+const secret = process.env.SECRET_TOKEN || 'self love';
+
+const Authentication = {
+
+  /**
+   * verifyToken - Verifies if a token supplied or not is valid
+   *
+   * @param  {object} req  Request object
+   * @param  {object} res  Response object
+   * @param  {function} next callback to the function
+   * @returns {object}      Response status
+   */
+  verifyToken(req, res, next) {
+    const token = req.headers.authorization || req.headers['x-access-token'];
+    if (!token) {
+      return res.status(401).send({ message: 'Authentication required to access this route!' });
+    }
+
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: 'Authentication failed due to invalid token!' });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+};
+
+module.exports = Authentication;
