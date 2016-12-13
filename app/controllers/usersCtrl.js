27a2f0a3310ca272d6734a3fb42667fc8ec885ff
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-13 09:58:48 +0100

    Take all user fields and set all the specific values accordingly

--- app/controllers/usersCtrl.js
+++ app/controllers/usersCtrl.js
@@ -3,6 +3,26 @@ import db from '../models';
 
 const secret = process.env.SECRET || 'jump drop mobs kicking it in';
 
+
+const userFields = (user) => {
+  const fields = {
+    id: user.id,
+    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    RoleId: user.RoleId,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
+  };
+
+  return fields;
+};
+
+
+/**
+ * Users controller
+ */
 const userCtrl = {
 
 };
