6d6554e201322ace30c1e9f87d75cea0f2d6a6bf
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-13 10:05:41 +0100

    Controller to get all users

--- app/controllers/usersCtrl.js
+++ app/controllers/usersCtrl.js
@@ -4,12 +4,18 @@ import db from '../models';
 const secret = process.env.SECRET || 'jump drop mobs kicking it in';
 
 
-const userFields = (user) => {
+/**
+ * All user fields and set values to the
+ * specific fields
+ * @param {Object}  user object
+ * @returns {Object} user fields object
+ */
+const allUserFields = (user) => {
   const fields = {
     id: user.id,
     username: user.username,
     firstName: user.firstName,
-    lastName: user.lastName,
+    lastNamse: user.lastName,
     email: user.email,
     RoleId: user.RoleId,
     createdAt: user.createdAt,
@@ -24,7 +30,28 @@ const userFields = (user) => {
  * Users controller
  */
 const userCtrl = {
-
+  /**
+   * Grab all users with their fields
+   * @param {Object} req - Request object
+   * @param {Object} res - Response object
+   * @returns {Void} Returns Void
+   */
+  allUsers(req, res) {
+    db.User.findAll({
+      fields: [
+        'id',
+        'username',
+        'firstName',
+        'lastName',
+        'email',
+        'RoleId',
+        'createdAt',
+        'updatedAt'
+      ]
+    }).then((users) => {
+      res.send(users);
+    });
+  },
 };
 
 module.exports = userCtrl;
