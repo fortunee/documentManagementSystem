c21c8d96df8946491742da6157de498a32ee7eb6
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-14 10:19:58 +0100

    Refactor types route to use controllers and authentication

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,14 @@
 import userRoute from './usersRoute';
 import docRoute from './docsRoute';
 import roleRoute from './rolesRoute';
+import typeRoute from './typesRoute';
 
 // Configure routes
 const routes = (router) => {
   userRoute(router);
   docRoute(router);
   roleRoute(router);
+  typeRoute(router);
 };
 
 export default routes;
--- app/routes/typesRoute.js
+++ app/routes/typesRoute.js
@@ -1,12 +1,19 @@
 /**
  * Import types controllers and authentication middlewares.
  */
-import typesCtrl from '../../app/controllers/rolesCtrl';
+import typesCtrl from '../../app/controllers/typesCtrl';
 import Authentication from '../../app/middlewares/auth';
 
 
-const rolesRoute = (router) => {
+const typesRoute = (router) => {
+  router.route('/types')
+    .get(Authentication.verifyToken, typesCtrl.allTypes)
+    .post(Authentication.verifyToken, typesCtrl.createType);
 
+  router.route('/types/:id')
+    .get(Authentication.verifyToken, typesCtrl.getType)
+    .put(Authentication.verifyToken, typesCtrl.editType)
+    .delete(Authentication.verifyToken, typesCtrl.deleteType);
 };
 
-export default rolesRoute;
+export default typesRoute;
