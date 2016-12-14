3d7a99b957a44ac6f7579921d58e5fdd2a53e5e4
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-12-14 10:05:56 +0100

    Set up types route

---
+++ app/routes/typesRoute.js
@@ -0,0 +1,12 @@
+/**
+ * Import types controllers and authentication middlewares.
+ */
+import typesCtrl from '../../app/controllers/rolesCtrl';
+import Authentication from '../../app/middlewares/auth';
+
+
+const rolesRoute = (router) => {
+
+};
+
+export default rolesRoute;
